41c6838b28462c2091e00ebf71d5afcc01563deb
GB81160 <116102042+GB81160@users.noreply.github.com>
2025-06-26 17:44:16 +0200

    Use constant for login slug

--- wpsoluces-core/Modules/LoginRedirect/Model.php
+++ wpsoluces-core/Modules/LoginRedirect/Model.php
@@ -4,6 +4,7 @@ namespace WPSolucesCore\Modules\LoginRedirect;
 defined( 'ABSPATH' ) || exit;
 
 class Model {
+       public const DEFAULT_SLUG = 'connect';
         public const OPTION_ACTIVE  = 'wpsc_lr_active';
         public const OPTION_SLUG    = 'wpsc_lr_slug';
         public const OPTION_FLUSHED = 'wpsc_lr_rewrite_flushed';
@@ -12,9 +13,9 @@ class Model {
                 return (bool) get_option( self::OPTION_ACTIVE, 0 );
         }
 
-        public static function slug(): string {
-                $slug = trim( (string) get_option( self::OPTION_SLUG, 'connect' ) );
-                return $slug !== '' ? $slug : 'connect';
-        }
+       public static function slug(): string {
+               $slug = trim( (string) get_option( self::OPTION_SLUG, self::DEFAULT_SLUG ) );
+               return $slug !== '' ? $slug : self::DEFAULT_SLUG;
+       }
 
 }
